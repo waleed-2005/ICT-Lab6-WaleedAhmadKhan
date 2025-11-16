e9ac98883a9752d64062ae68d7451d785da3ffda
Waleed27 <waleedahmad.2005@icloud.com>
2025-11-17 02:51:47 +0500

    Added a simple program

---
+++ noteGit.cpp
@@ -0,0 +1,16 @@
+/*
+Git and GitHub Note:
+
+Git is a distributed version control system that tracks changes in code.
+It helps developers maintain project history and collaborate efficiently.
+
+GitHub is a cloud-based hosting platform for Git repositories.
+It enables remote storage, collaboration, and code sharing among developers.
+*/
+#include <iostream>
+using namespace std;
+int main (){
+    cout<<"ICT Lab 6"<<endl;
+
+return 0;
+}
